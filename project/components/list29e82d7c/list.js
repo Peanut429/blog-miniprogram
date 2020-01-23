ef43a29b6d15c3d0e197af616c34c2310f52b302
Peanut <18915000292@163.com>
2020-01-23 21:56:19 +0800

    初始化项目

---
+++ project/components/list29e82d7c/list.js
@@ -0,0 +1,5 @@
+
+var window = window || {};
+
+window["webpackJsonp"] = require("../../bundle.js");
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{160:function(t,i,n){"use strict";n.r(i),function(t){t.currentModuleId="m197c1685",t.currentCtor=Component,t.currentSrcMode="wx",t.currentInject={moduleId:"m197c1685",render:function(){var t={};return this.__travel((t.mpxShow=[this.mpxShow,"mpxShow"],this.mpxShow||void 0===(t.mpxShow=[this.mpxShow,"mpxShow"],this.mpxShow)?"":"display:none;"),[]),this.__iterate((t.listData=[this.listData,"listData"],this.listData),function(t,i){}.bind(this)),t}};n(96);t.currentModuleId}.call(this,n(12))},96:function(t,i,n){"use strict";var o=n(31);Object(o.a)({data:{listData:["手机","电视","电脑"]}})}},[[160,0]]]);
---
+++ project/pages/index.js
@@ -0,0 +1,5 @@
+
+var window = window || {};
+
+window["webpackJsonp"] = require("../bundle.js");
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{159:function(t,e,n){"use strict";n.r(e),function(t){t.currentModuleId="m3e28ffd5",t.currentCtor=Component,t.currentSrcMode="wx",t.currentInject={moduleId:"m3e28ffd5",render:function(){var t={};return this.__travel((t.mpxPageStatus=[this.mpxPageStatus,"mpxPageStatus"],this.mpxPageStatus),[],!0),t}};n(95);t.currentModuleId}.call(this,n(12))},95:function(t,e,n){"use strict";var r=n(31);Object(r.b)({onLoad:function(){}})}},[[159,0]]]);
